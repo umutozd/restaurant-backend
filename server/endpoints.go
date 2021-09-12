d5adbe90db0833f8e87bf2973c9d9763ff07747a
Umut Özdoğan <umut@otsimo.com>
2021-09-12 14:59:36 +0300

    server: add http method checks to endpoints

--- server/endpoints.go
+++ server/endpoints.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/sirupsen/logrus"
+	"github.com/umutozd/restaurant-backend/types"
 )
 
 type loggerKey struct{}
@@ -59,29 +60,41 @@ func (s *server) getLoggerFromRequest(r *http.Request) *logrus.Entry {
 // methods will be able to retrieve their own logger objects from context.
 func (s *server) getHandler(e endpoint) http.HandlerFunc {
 	var h http.HandlerFunc
+	var method string
 	switch e {
 	case EndpointListMenu:
 		h = s.ListMenu
+		method = http.MethodGet
 	case EndpointCreateMenuItem:
 		h = s.CreateMenuItem
+		method = http.MethodPost
 	case EndpointListMenuItems:
 		h = s.ListMenuItems
+		method = http.MethodGet
 	case EndpointUpdateMenuItem:
 		h = s.UpdateMenuItem
+		method = http.MethodPost
 	case EndpointDeleteMenuItem:
 		h = s.DeleteMenuItem
+		method = http.MethodDelete
 	case EndpointCreateCategory:
 		h = s.CreateCategory
+		method = http.MethodPost
 	case EndpointListCategories:
 		h = s.ListCategories
+		method = http.MethodGet
 	case EndpointUpdateCategory:
 		h = s.UpdateCategory
+		method = http.MethodPost
 	case EndpointDeleteCategory:
 		h = s.DeleteCategory
+		method = http.MethodDelete
 	case EndpointUpdateCart:
 		h = s.UpdateCart
+		method = http.MethodPost
 	case EndpointUpdateCartItemsStatus:
 		h = s.UpdateCartItemsStatus
+		method = http.MethodPost
 	default:
 		return func(w http.ResponseWriter, r *http.Request) {
 			logger := s.getLoggerFromRequest(r)
@@ -94,6 +107,11 @@ func (s *server) getHandler(e endpoint) http.HandlerFunc {
 
 	return func(w http.ResponseWriter, r *http.Request) {
 		logger := logrus.WithField("endpoint", e)
+		if r.Method != method {
+			s.writeError(w, logger, types.Errf(types.ERR_WRONG_HTTP_METHOD, "for endpoint %q, http method must be %s", e, method))
+			return
+		}
+
 		r = r.WithContext(context.WithValue(r.Context(), loggerKey{}, logger))
 		h(w, r)
 	}
